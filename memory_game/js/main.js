1211a5d6493645c9c2320c66179e1d3ae2e74dc6
Vincent <Blooki@Vincents-MacBook-Air.local>
2018-11-27 16:15:24 -0500

    added to game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,19 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+for(var i = 0; i < cardsInPlay.length; i++) {
+	console.log("User flipped " + cardsInPlay[i]);
+}
+if(cardsInPlay.length === 2) {
+	if(cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	}
+	else {
+		alert("Sorry, try again.")
+	}
+}
