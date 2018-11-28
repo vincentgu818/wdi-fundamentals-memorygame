c450ed230c81d58ed992a2c39267b7b953684843
Vincent <Blooki@Vincents-MacBook-Air.local>
2018-11-28 14:59:45 -0500

    organized code into functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,15 +1,7 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-for(var i = 0; i < cardsInPlay.length; i++) {
-	console.log("User flipped " + cardsInPlay[i]);
-}
-if(cardsInPlay.length === 2) {
+var checkForMatch = function() {
 	if(cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	}
@@ -17,3 +9,14 @@ if(cardsInPlay.length === 2) {
 		alert("Sorry, try again.")
 	}
 }
+
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if(cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+};
+
+flipCard(0);
+flipCard(2);
