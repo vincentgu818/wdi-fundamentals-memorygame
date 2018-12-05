804c6e46d3215be51646b060b822c9dbcd871b3c
Vincent <Blooki@Vincents-MacBook-Air.local>
2018-12-05 15:38:25 -0500

    Smoothed card flipping functionality and added features for playing multiple games.

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -20,6 +20,7 @@ var cards = [
 	cardImage: 'images/king-of-diamonds.png'
 }
 ];
+
 var cardsInPlay = [];
 
 var checkForMatch = function() {
@@ -29,17 +30,38 @@ var checkForMatch = function() {
 	else {
 		alert("Sorry, try again.")
 	}
+};
+
+var resetBoard = function() {
+	var cardElements = document.getElementsByTagName('img');
+	for(var i = 0; i < cardElements.length; i++) {
+		cardElements[i].setAttribute('src','images/back.png');
+	}
 }
 
 var flipCard = function() {
-	var cardId = this.getAttribute('data-id');
-	console.log("User flipped " + cards[cardId].rank + " of " + cards[cardId].suit + " (" + cards[cardId].cardImage + ")");
-	cardsInPlay.push(cards[cardId].rank);
-	this.setAttribute('src',cards[cardId].cardImage);
-	if(cardsInPlay.length === 2) {
-		checkForMatch();
+	// A card is being flipped faceup.
+	if(this.getAttribute('src') === 'images/back.png') {
+		var cardId = this.getAttribute('data-id');
+		console.log("User flipped " + cards[cardId].rank + " of " + cards[cardId].suit + " (" + cards[cardId].cardImage + ")");
+		cardsInPlay.push(cards[cardId].rank);
+		
+		// Check for match if two cards are in play.
+		if(cardsInPlay.length === 2) {
+			checkForMatch();
+			cardsInPlay.pop();
+			cardsInPlay.pop();
+		} 
+		// Otherwise, one card in play means a new round with a new board.
+		else {
+			resetBoard();
+		}
+		this.setAttribute('src',cards[cardId].cardImage);
+	} 
+	// User flipping a card facedown also means a new round with a new board.
+	else {
 		cardsInPlay.pop();
-		cardsInPlay.pop();
+		resetBoard();
 	}
 };
 
@@ -51,6 +73,6 @@ var createBoard = function() {
 		cardElement.addEventListener('click',flipCard);
 		document.getElementById('game-board').appendChild(cardElement);
 	}
-}
+};
 
 createBoard();
